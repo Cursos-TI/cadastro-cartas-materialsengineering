10ed4dc9e3559ff46247602528ba04821b8d9677
Programming in C <90535859+materialsengineering@users.noreply.github.com>
2026-07-14 15:50:29 +0000

    cadastro da cartaA incompleto continue

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,17 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  // Cadastramento da 1ª Carta
+  char Estado_A[] = "A";
+  char cidade_1[5] = "A01";
+  char cidade_2[5] = "A02";
+  char cidade_3[5] = "A03";
+  char cidade_4[5] = "A04";
+  int populacao_A01 = 20000000;
+  int populacao_A02 = 50000000;
+  int populacao_A03 = 15000000;
+  int populacao_A04 = 75000000;
+
 
   // Área para entrada de dados
 
