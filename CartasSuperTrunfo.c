4799773922a32ac805a6fda32ea6c83c30a9bd77
Programming in C <90535859+materialsengineering@users.noreply.github.com>
2026-07-14 19:32:22 +0000

    correção entrada

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,21 +6,43 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  // Cadastramento da 1ª Carta
-  char Estado_A[] = "A";
-  char cidade_1[5] = "A01";
-  char cidade_2[5] = "A02";
-  char cidade_3[5] = "A03";
-  char cidade_4[5] = "A04";
+  // Cadastramento da 1ª e 2ª Carta
+  char codEstado_A[] = 'A';
+  char codEstado_B[] = 'B';
+  char codcidade_1A[5] = 'A01';
+  char codcidade_2A[5] = 'A02';
+  char codcidade_1B[5] = 'B01';
+  char codcidade_2B[5] = 'B02';
   int populacao_A01 = 20000000;
   int populacao_A02 = 50000000;
-  int populacao_A03 = 15000000;
-  int populacao_A04 = 75000000;
+  int populacao_B01 = 15000000;
+  int populacao_B02 = 75000000;
+  float area_A01 = 353.90;
+  float area_A02 = 134.87;
+  float area_B01 = 122.35;
+  float area_B02 = 100.35;
+  float pib_A01 = 230.15;
+  float pib_A02 = 144.35;
+  float pib_B01 = 123.56;
+  float pib_B02 = 178.98;
+  int pturismo_01A = 34;
+  int pturismo_02A = 20;
+  int pturismo_01B = 15;
+  int pturismo_02B = 18;
+  char estado_A[10] = "Ceará";
+  char estado_B[10] = "Paraíba";
+  char cidade_01[10] = "Fortaleza";
+  char cidade_02[10] = "Maracanaú";
+  char cidade_B1[10] = "Patos";
+  char cidade_B2[10] = "Umari";
 
 
   // Área para entrada de dados
 
   // Área para exibição dos dados da cidade
+  printf("A carta 1\n");
+  printf("código Estado %c, codigo cidade %s\n",codEstado_A, codcidade_1A);
+  
 
 return 0;
 } 
